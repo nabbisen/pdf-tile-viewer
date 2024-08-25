bbc1fa23fcf0fdd8ab1cbe678b2dd1370cf55ba7
nabbisen <nabbisen@scqr.net>
2024-08-25 12:57:09 +0900

    fix #35 global css; fix #34 button click effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
   // import Header from '../layouts/Header.svelte'
   // import Footer from '../layouts/Footer.svelte'
   import Toast from '../layouts/Toast.svelte'
+
+  import '../app.css'
 </script>
 
 <!-- <header><Header /></header> -->
