f0b2eb376083279ede9503947e8d47f02949f9d8
nabbisen <nabbisen@scqr.net>
2024-08-28 17:16:15 +0900

    fix #76 share app button

--- src/pages/Dashboard/Header.svelte
+++ src/pages/Dashboard/Header.svelte
@@ -1,7 +1,32 @@
+<script lang="ts">
+  import Tooltip from '../../components/Tooltip.svelte'
+  import { successToast, errorToast } from '../../stores/components/toast'
+
+  const SHARE_THIS_APP_MESSAGES: string =
+    'PDF Tile Viewer : https://github.com/nabbisen/pdf-tile-viewer/releases'
+
+  async function shareThisAppOnClick() {
+    try {
+      await navigator.clipboard.writeText(SHARE_THIS_APP_MESSAGES)
+      successToast('App info copied to clipboard.', 2100)
+    } catch (err) {
+      errorToast('Failed to copy to clipboard.', 1400)
+    }
+  }
+</script>
+
 <header>
   <div class="logo">
-    <img src="/logo.png" alt="" />
+    <img src="/logo.png" alt="logo" />
     <h1><span>PDF</span> <span>Tile</span> <span>Viewer</span></h1>
+  </div>
+  <div class="share-this-app">
+    <Tooltip messages="App info is copied to clipboard" position="right">
+      <button on:click={shareThisAppOnClick}>
+        <div>Share</div>
+        <span>this app</span>
+      </button>
+    </Tooltip>
   </div>
 </header>
 
@@ -9,6 +34,7 @@
   header {
     position: relative;
     z-index: 10000;
+    display: flex;
   }
 
   @keyframes logo-bg {
@@ -45,7 +71,7 @@
   h1 {
     font-size: 1.8rem;
     padding: 0;
-    margin: 0;
+    margin: 0 0 0 0.6rem;
     display: flex;
     align-items: center;
   }
@@ -60,5 +86,30 @@
   }
   h1 span:nth-child(3) {
     color: #939393;
+  }
+
+  .share-this-app {
+    margin-left: 1.5rem;
+    margin-top: 0.3rem;
+  }
+  .share-this-app button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .share-this-app button div {
+    width: 2.2rem;
+    height: 2.2rem;
+    background-color: hsl(185, 63%, 93%);
+    color: hsl(185, 87%, 33%);
+    border-radius: 50%;
+    font-size: 0.7rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .share-this-app button span {
+    color: #878787;
+    font-size: 0.57rem;
   }
 </style>
