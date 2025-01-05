f8466625931eba7711d482b980b1ebe4f97db7fd
nabbisen <nabbisen@scqr.net>
2025-01-05 15:09:00 +0000

    prerender deactivated temporarily (#90)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,7 @@
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-export const prerender = 'auto'
+// todo: set false at prerender temporarily
+//       route parameter `filepath` as path parameter, store or query parameter (ssr required...)
+export const prerender = false
 export const ssr = false
