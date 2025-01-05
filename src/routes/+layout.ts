2e4dd31262fb26756b2f9ded6eae847c3dfbff70
nabbisen <nabbisen@scqr.net>
2025-01-06 00:17:56 +0900

    limit prerendered bc/ filepath is currently dynamically set as route param

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,5 @@
 // Tauri doesn't have a Node.js server to do proper SSR
 // so we will use adapter-static to prerender the app (SSG)
 // See: https://v2.tauri.app/start/frontend/sveltekit/ for more info
-// todo: set false at prerender temporarily
-//       route parameter `filepath` as path parameter, store or query parameter (ssr required...)
-export const prerender = false
+export const prerender = true
 export const ssr = false
--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,11 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    // todo: add prerender.entries temporarily
+    //       should route parameter `filepath` be as either path parameter, store or query parameter (ssr required...) ?
+    prerender: {
+      entries: ['/dashboard'],
+    },
   },
 };
 
